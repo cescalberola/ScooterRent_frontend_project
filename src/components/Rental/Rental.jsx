91a2fc26640b3b04ef8227e60c46235c7ecef821
Francesc Alberola <f.alberola@gmail.com>
2023-11-29 10:02:12 +0100

    sombras rental

--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ShoppingCartOutlined } from '@ant-design/icons';
+import { ShoppingCartOutlined,StarOutlined, HeartOutlined } from '@ant-design/icons';
 import { Card, Row, Col } from 'antd';
 const { Meta } = Card;
 import "./Rental.scss"
@@ -13,7 +13,9 @@ const Rental = () => (
           style={{ width: 300 }}
           cover={<img alt="example" src="https://imgs.search.brave.com/1B2f8BpDVtHqJBI21MaHNqdbT8BMShLhT--0YedXOfk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG93ZXJwbGFuZXRv/bmxpbmUuY29tL2Nk/bmFzc2V0cy9jZWNv/dGVjX2JvbmdvX3Nl/cmllX2FfcGx1c19t/YXhfNDVfY29ubmVj/dGVkX25lZ3JvX3Bh/dGluZXRlX2VsZWN0/cmljb18wMV9sLmpw/Zw" />}
           actions={[
-            <ShoppingCartOutlined />
+            <ShoppingCartOutlined />,
+            <StarOutlined />,
+            <HeartOutlined />
           ]}
         >
           <Meta
@@ -27,7 +29,9 @@ const Rental = () => (
           style={{ width: 300 }}
           cover={<img alt="example" src="https://imgs.search.brave.com/o723oz3zcZuSxJ4b1HY_IFHwZN_KTOOugsFcHbJefVk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/MzF3NThSOGtZd0wu/anBn" />}
           actions={[
-            <ShoppingCartOutlined />
+            <ShoppingCartOutlined />,
+            <StarOutlined />,
+            <HeartOutlined />
           ]}
         >
           <Meta
@@ -41,7 +45,9 @@ const Rental = () => (
           style={{ width: 300 }}
           cover={<img alt="example" src="https://imgs.search.brave.com/sndaINwQ-maGKuDCohiYwqSbW2i2c50nhVgb2i6qExo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wY2Jv/eC52dGV4YXNzZXRz/LmNvbS9hcnF1aXZv/cy9pZHMvNDg4NzI4/LTgwMC1hdXRvP3Y9/NjM3ODk3MzIxODQx/MjMwMDAwJndpZHRo/PTgwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ" />}
           actions={[
-            <ShoppingCartOutlined />
+            <ShoppingCartOutlined />,
+            <StarOutlined />,
+            <HeartOutlined />
           ]}
         >
           <Meta
@@ -59,7 +65,9 @@ const Rental = () => (
           style={{ width: 300 }}
           cover={<img alt="example" src="https://imgs.search.brave.com/pK2GSD2vJyjIScMGiNCWaAzKPKolyrGXydA8xpP_9oc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtcGVwcGVyLmRl/YWxhYnMuY29tL3Ro/cmVhZHMvcmF3LzVL/NW40LzI0MDE2NTdf/MS9yZS83Njh4NzY4/L3F0LzYwLzI0MDE2/NTdfMS5qcGc" />}
           actions={[
-            <ShoppingCartOutlined />
+            <ShoppingCartOutlined />,
+            <StarOutlined />,
+            <HeartOutlined />
           ]}
         >
           <Meta
@@ -73,7 +81,9 @@ const Rental = () => (
           style={{ width: 300 }}
           cover={<img alt="example" src="https://imgs.search.brave.com/7nhmi-gYPc38ge6dcOqjeA8JU2tzJEubjS1A0YGaQOo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9saXZl/LnN0YXRpY2ZsaWNr/ci5jb20vNjU1MzUv/NDk2MDAyMzY1NTZf/ZjJjY2U5YmMxMV96/LmpwZw" />}
           actions={[
-            <ShoppingCartOutlined />
+            <ShoppingCartOutlined />,
+            <StarOutlined />,
+            <HeartOutlined />
           ]}
         >
           <Meta
@@ -87,7 +97,9 @@ const Rental = () => (
           style={{ width: 300 }}
           cover={<img alt="example" src="https://imgs.search.brave.com/HatxWZ6WQMr2Be6seiyfDZmMX6SvN6qjJtcA-vFUnyQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzEwa2x5U1JxUkwu/anBn" />}
           actions={[
-            <ShoppingCartOutlined />
+            <ShoppingCartOutlined />,
+            <StarOutlined />,
+            <HeartOutlined />
           ]}
         >
           <Meta
