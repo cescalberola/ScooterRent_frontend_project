4a6a4754585a3adc1cb77fbd35c0072fa84c5f3d
Francesc Alberola <f.alberola@gmail.com>
2023-12-01 08:38:08 +0100

    home again

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.scss';
 const Home = () => {
   return (
     <div className='banner-container'>
-      <h1 className='banner-text'>Bienvenidos a VAL Scooters</h1>
+      <h1 className='banner-text'>Bienvenidos a VAL scooters</h1>
     </div>
   );
 };
